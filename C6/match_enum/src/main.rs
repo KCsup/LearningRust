896be5fa66dc50fdc84444a17be3f5b4af886aea
KCsup <kcsup05@gmail.com>
2023-04-30 18:53:39 -0400

    if let testing

--- C6/match_enum/src/main.rs
+++ C6/match_enum/src/main.rs
@@ -2,6 +2,11 @@ fn main() {
     let coin_var = Coin::Quarter(State::AmongUs);
 
     println!("Coin value: {}", get_coin_value(&coin_var));
+
+    // later testing
+    if let Coin::Quarter(state) = coin_var {
+        println!("{:?}", state);
+    }
 }
 
 #[derive(Debug)]
