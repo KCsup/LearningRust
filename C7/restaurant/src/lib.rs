1dfa921ea9a95549f8095769bb864439c63c4b24
KCsup <kcsup05@gmail.com>
2023-05-06 02:43:17 -0400

    More module and scope practice.

--- C7/restaurant/src/lib.rs
+++ C7/restaurant/src/lib.rs
@@ -1,5 +1,30 @@
 mod front_of_house {
 
+    pub struct Breakfast {
+        pub toast: String,
+        fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn certain_season(toast: &str) -> Self {
+            // Associated function; No '&self' as param
+
+            // Needs associated function for struct instance since one of the
+            // values is private
+
+            Breakfast {
+                toast: toast.to_string(),
+                fruit: String::from("SomeFruit :)"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        // All enum variants are public when the parent is set so
+        Soup,
+        Salad,
+    }
+
     fn get_front_name() {}
 
     pub mod hosting {
@@ -21,9 +46,28 @@ mod front_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
 
     // same applies for local path
     front_of_house::hosting::add_to_waitlist();
+
+    // Or with the use scope
+    hosting::add_to_waitlist();
+
+    // Order
+
+    let mut meal = front_of_house::Breakfast::certain_season("Wheat");
+
+    meal.toast = "OtherToastType".to_string();
+
+    println!("Now getting {}, with some fruit", meal.toast);
+    // Cannot access 'meal.fruit' since it's not public
+
+    // Appetizers :)
+
+    let app_one = front_of_house::Appetizer::Soup;
+    let app_two = front_of_house::Appetizer::Salad;
 }
